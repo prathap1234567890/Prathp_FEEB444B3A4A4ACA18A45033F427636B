6b93ac2728535c776bcb98174fad181f0f2a0d50
prathap1234567890 <145096893+prathap1234567890@users.noreply.github.com>
2023-09-15 12:12:43 +0530

    Add files via upload

---
+++ fact.py
@@ -0,0 +1,15 @@
+
+def factorial(n):
+ 
+
+    if n < 1:
+        return 1
+ 
+  
+    return n * factorial(n - 1)
+ 
+ 
+if __name__ == '__main__':
+ 
+    n = 5
+    print(f'The Factorial of {n} is', factorial(n))
